120f6da09f86a0c697a093e36a962f4a7e517ff5
Jose Manuel Mira Martínez <josema.mira@gmail.com>
2022-02-09 18:48:32 +0100

    Update pivot.sql

--- pivot.sql
+++ pivot.sql
@@ -1,11 +1,14 @@
+-- this query are using a subquery (t) to extract year data and filter by one observatory. In this case we are using the #1 observatory (obs=1)
+-- You must create as many columns as there are years in the table
+
 SELECT DISTINCT anyo, group_concat(valor, ',') OVER (PARTITION BY anyo) AS group_concat
 FROM (select substr(fecha,1,4) as anyo, valor from obs where obs=1) t;
 
 SELECT 
        MAX(CASE WHEN "anyo" == '1951' THEN valor END) as 'a1951',
        MAX(CASE WHEN "anyo" == '1952' THEN valor END) as 'a1952',
-       MAX(CASE WHEN "anyo" == '1953' THEN valor END) as 'a1953',
-	   MAX(CASE WHEN "anyo" == '1954' THEN valor END) as 'a1954'	   
+       MAX(CASE WHEN "anyo" == '1953' THEN valor END) as 'a1953'
+       -- add new years here
 FROM (select t.*,
              row_number() over (partition by anyo ) as seq
       from (select substr(fecha,1,4) as anyo, valor from observaciones where obs=1) t
