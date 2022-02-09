7b3b84e3926a9a8a33429fd633c5e9915f5a364a
Jose Manuel Mira Martínez <josema.mira@gmail.com>
2022-02-09 19:13:20 +0100

    Update pivot.sql

--- pivot.sql
+++ pivot.sql
@@ -5,7 +5,8 @@ SELECT
        MAX(CASE WHEN "anyo" == '1951' THEN valor END) as 'a1951',
        MAX(CASE WHEN "anyo" == '1952' THEN valor END) as 'a1952',
        MAX(CASE WHEN "anyo" == '1953' THEN valor END) as 'a1953'
-       -- add new years here
+	   -- add new years here
+	     
 FROM (select t.*,
              row_number() over (partition by anyo ) as seq
       from (select substr(fecha,1,4) as anyo, valor from observaciones where obs=1) t
