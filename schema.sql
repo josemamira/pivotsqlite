be8d3cef251492451ac36d22f8993fef8bf66468
Jose Manuel Mira Martínez <josema.mira@gmail.com>
2022-02-09 18:41:51 +0100

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- in english
+CREATE TABLE "observations" (
+	"date"	TEXT,
+	"value"	REAL,
+	"obs"	INTEGER
+);
+
+-- spanish
+CREATE TABLE "observaciones" (
+	"fecha"	TEXT,
+	"valor"	REAL,
+	"obs"	INTEGER
+);
